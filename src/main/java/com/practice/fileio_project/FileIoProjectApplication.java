d0135df2d3b5ca8caf278453774ab8fc00a0f206
AndreeaBrumaru <andreea.brumaru@metro.digital>
2022-09-02 09:57:17 +0300

    Initial commit

---
+++ src/main/java/com/practice/fileio_project/FileIoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.practice.fileio_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileIoProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileIoProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/practice/fileio_project/FileIoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.fileio_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileIoProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
