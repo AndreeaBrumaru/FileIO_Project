317cc227b9ba86f0ecb9fa2fe1b3d8a1c4b1d658
AndreeaBrumaru <andreea.brumaru@metro.digital>
2022-09-02 13:17:30 +0300

    Made prototype

--- src/main/java/com/practice/fileio_project/FileIoProjectApplication.java
+++ src/main/java/com/practice/fileio_project/FileIoProjectApplication.java
@@ -1,9 +1,12 @@
 package com.practice.fileio_project;
 
+import com.practice.fileio_project.property.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({FileStorageProperties.class})
 public class FileIoProjectApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/practice/fileio_project/exception/FileStorageException.java
@@ -0,0 +1,15 @@
+package com.practice.fileio_project.exception;
+
+
+public class FileStorageException extends RuntimeException{
+
+    public FileStorageException(String message)
+    {
+        super(message);
+    }
+
+    public FileStorageException(String message, Throwable cause)
+    {
+        super(message, cause);
+    }
+}
